0a4b4bdf35ef905bd64ad6b5889130a4169b2e57
AdityaBhatnagar2107 <adityabhatnagar2105@gmail.com>
2026-02-28 15:58:19 +0530

    FINAL DEPLOY: PWA Mobile App Setup, Premium Faculty UI, and Official Icon

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,3 @@
-// This tells the phone's browser that this is an installable PWA
 self.addEventListener('install', (e) => {
     console.log('[KNMIET App] Background Engine Installed');
     self.skipWaiting();
@@ -9,6 +8,5 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    // Allows the app to function normally by passing network requests through
     e.respondWith(fetch(e.request).catch(() => console.log("Network error")));
 });

e5438dda50ee0a51ee8e38e022dca2c5605864a2
AdityaBhatnagar2107 <adityabhatnagar2105@gmail.com>
2026-03-17 23:20:58 +0530

    Fix database engine for PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -6,10 +6,8 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL="postgresql://knmiet_db_user:LPsYZcZX70OUqScLHufnTZRjAH7H5W9A@dpg-d6e8h27pm1nc73aasstg-a/knmiet_db"
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./attendance.db"
 
-#engine = create_engine(SQLALCHEMY_DATABASE_URL)
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
